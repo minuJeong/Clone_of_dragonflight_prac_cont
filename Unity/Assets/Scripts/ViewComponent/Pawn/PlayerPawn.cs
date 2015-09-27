d81f9c5fab9078baa595393712034293f2a0332f
minuj <minu.hanwool@gmail.com>
2015-09-27 17:43:38 +0900

    Add input control system / set project managed by force text mode

--- Unity/Assets/Scripts/ViewComponent/Pawn/PlayerPawn.cs
+++ Unity/Assets/Scripts/ViewComponent/Pawn/PlayerPawn.cs
@@ -3,4 +3,59 @@ using System.Collections;
 
 public class PlayerPawn : Pawn
 {
+    #region const
+    private const float SHOOT_DELAY_TIME = 0.25F;
+    #endregion
+
+    #region private:
+    private WaitForSeconds m_WaitTime;
+    private bool m_IsShooting;
+    #endregion
+
+    #region public:
+    public void StartShoot()
+    {
+        m_IsShooting = true;
+    }
+    
+    public void StopShoot()
+    {
+        m_IsShooting = false;
+    }
+    
+    public void MoveBy(Vector3 amount)
+    {
+        transform.position += amount;
+    }
+    #endregion
+
+    #region private:
+    private void Start()
+    {
+        // activate shoot by default
+        StartShoot();
+
+        StartCoroutine("ShootCycle");
+
+        m_WaitTime = new WaitForSeconds(SHOOT_DELAY_TIME);
+    }
+
+    private IEnumerator ShootCycle()
+    {
+        while (true)
+        {
+            if (m_IsShooting)
+            {
+                Shoot();
+            }
+
+            yield return m_WaitTime;
+        }
+    }
+
+    private void Shoot()
+    {
+
+    }
+    #endregion
 }
