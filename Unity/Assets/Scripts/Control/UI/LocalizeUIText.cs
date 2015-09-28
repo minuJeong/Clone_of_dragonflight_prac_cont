68de49dc87d6077bd13f154376f17e99665ff0de
minuj <minu.hanwool@gmail.com>
2015-09-28 11:20:13 +0900

    Add null check for string table

--- Unity/Assets/Scripts/Control/UI/LocalizeUIText.cs
+++ Unity/Assets/Scripts/Control/UI/LocalizeUIText.cs
@@ -5,6 +5,8 @@ using System.Collections;
 [ExecuteInEditMode]
 public class LocalizeUIText : MonoBehaviour
 {
+    private const string LAN_CODE = "KR";
+
     public string LocalizeKey = "";
     private Text text;
 
@@ -23,8 +25,11 @@ public class LocalizeUIText : MonoBehaviour
     {
         if ("" != LocalizeKey)
         {
-            text = GetComponent<Text>();
-            text.text = StringTable.Data [LocalizeKey].ToString();
+            if ((StringTable.Data[LAN_CODE] as IDictionary).Contains (LocalizeKey))
+            {
+                text = GetComponent<Text>();
+                text.text = StringTable.Data [LAN_CODE][LocalizeKey].ToString();
+            }
         }
     }
 }
